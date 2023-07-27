a0c949778db0d0b9c7244c4c78ea5b827ae9d479
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-07-27 13:49:55 +0200

    operador ternario

---
+++ 15-Operador-Ternario.js
@@ -0,0 +1,8 @@
+// Operador Ternario (CONDICION)? SI ES VERDADERO : SI ES FALSO;
+
+let number = 10;
+// Guardamos el resultado del la condicion dentro de la variavel resultado;
+let resultado = (number)? number < 20 : number > 20;
+
+// lo mostramos en consola 
+console.log(resultado); // retuena true ya q el number es menor q 20
