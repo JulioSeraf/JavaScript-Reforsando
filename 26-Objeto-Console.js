3440d274cf5ba731f6caac097ab83111b86a1a1c
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-08-02 16:19:28 +0200

    Objetos-Console

---
+++ 26-Objeto-Console.js
@@ -0,0 +1,29 @@
+//Objeto console 
+console.log(console);
+// Es el objeto que nos da acesso a la consola de la pagina, y viene con varios metodos de uso
+
+// METODO log(), carrega lo que asignamos a la consola, un geristro de lo que a passado en la aplicacion,
+console.log('Esta carregado!');
+
+// METODO error carrega un error a la consola
+console.error('Esto es una message de error');
+
+// METODO warn muestra un aviso a la consola
+console.warn('este es un aviso');
+
+// METODO info  mostra una informacio en la cosola
+console.info('Estas avisado');
+
+// variavel de teste
+let nombre = 'Julio',
+    edad = 26,
+    apellido = 'Serafim';
+
+// podemos llamar las variaveis por separado con varios consoles
+console.log(nombre);
+console.log(edad);
+console.log(apellido);
+// o lo simplificamas en un solo console.lo()
+console.log(nombre,apellido,edad);
+
+// tbam podemos criar un texto 
