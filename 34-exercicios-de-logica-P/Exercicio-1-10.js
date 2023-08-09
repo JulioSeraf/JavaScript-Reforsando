ef5ca0bb5431cd61810308b72fa6517a8f2e0171
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-08-09 17:48:54 +0200

    exercicio 1 contador terminado

---
+++ 34-exercicios-de-logica-P/Exercicio-1-10.js
@@ -0,0 +1,27 @@
+/*
+
+2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. */
+
+/*1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10. */
+function contador(){
+const text = document.getElementById('text'),
+res = document.getElementById('resCot'),
+but = document.querySelector('#but');
+    res.innerText = '';
+    let num = [1,2,3,4,5,6,7,8,9,0];
+    let valida = false;
+    for(let i in num){
+        if(text.value.includes(num[i])){
+            valida = true;
+        }
+    }
+    if(valida === true){
+        res.innerText = 'El texto no puede contener numeros';
+    }else if(text.value == ''){
+        res.innerText = 'Informe un texto para hacer la contagem';
+    }else if(text.value.length >= 1){
+        res.innerHTML = 'El texto contiene un total de ' + `<br><strong>${text.value.length}</strong><br>` + ' caracteres.';
+    };
+}
