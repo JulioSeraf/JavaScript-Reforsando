326faeff0343aa3930e60e6ab0de4082b9a02d8c
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-08-09 19:09:46 +0200

    Exercicio de recorte de texto Terminado

--- 34-exercicios-de-logica-P/Exercicio-1-10.js
+++ 34-exercicios-de-logica-P/Exercicio-1-10.js
@@ -5,6 +5,10 @@
 
 const text = document.getElementById('text'),
 res = document.getElementById('resCot');
+document.getElementById('limpiar').addEventListener('click',()=>{
+    text.value = '';
+})
+
 
 /*1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10. */
 
@@ -28,8 +32,25 @@ function contador(){
 
 /*2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola". */
 function cortar(){
-    
-}
+    res.innerHTML = '';
+    let diplayOn = document.getElementById('cortar');
+    let corIni = document.getElementById('inicio'),
+    corFim = document.getElementById('fim'),
+    butCort = document.getElementById('but-corte');
+    diplayOn.style.display = 'block';
+    butCort.addEventListener('click',()=>{
+        diplayOn.style.display = 'none';
+        console.log(corFim.value,corIni.value)
+        if(text.value === ''){
+            res.innerHTML = 'infome un texto a recortar'
+        }else if(corFim.value === ''){
+            res.innerHTML = 'informe el fim de corte';
+        }else{
+            res.innerHTML = text.value.slice(corIni.value,corFim.value)
+        };
+    });
+
+};
 
 /* 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].*/
 
