23df740bffea494d39b8bc3ae2b5a3b323b3e94d
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-08-21 16:28:17 +0200

    Termindo Callback 45

--- 46-callback.js
+++ 46-callback.js
@@ -3,8 +3,30 @@
 function cuadradoCallback(value,Callback){
     setTimeout(()=>{
         Callback(value, value*value)
-    },0|Math.random()*100)
+    },0|Math.random()*1000)
 }
-cuadradoCallback(0,(value,result)=>{
-    console.log("Inicia Callback")
-})
+cuadradoCallback(5,(value,result)=>{
+    console.log("Inicia Callback");
+    console.log(`El valor ${value} multiplicado por ${value} es ${result}`);
+    cuadradoCallback(10,(value,result)=>{
+        console.log(`El valor ${value} multiplicado por ${value} es ${result}`);
+        cuadradoCallback(23,(value,result)=>{
+            console.log(`El valor ${value} multiplicado por ${value} es ${result}`);
+            cuadradoCallback(20,(value,result)=>{
+                console.log(`El valor ${value} multiplicado por ${value} es ${result}`);
+            });
+        });
+    }); // Este seria el famoso callbackhell
+});
+
+// aqui podemos ver el LastIn firstOut = el ultimo en entrar el primero a salir. La function de sumar sale primero q la de cuadrador, por haver sido declarada por ultimo.
+
+function sumador(valor, callback){
+    setTimeout(()=>{
+        callback(valor, valor + valor);
+    }, Math.random()*1000)
+};
+
+sumador(3,(valor, result)=>{
+     console.log("El resultado de " + valor + " si  mesmo e " + result);
+});
