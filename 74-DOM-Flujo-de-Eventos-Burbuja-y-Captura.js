9904483b5e32c03be705de040af494387979f1ed
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-08-27 19:25:41 +0200

    74-DOM-Flujo-de-Eventos-Burbuja-y-Captura parte 2

--- 74-DOM-Flujo-de-Eventos-Burbuja-y-Captura.js
+++ 74-DOM-Flujo-de-Eventos-Burbuja-y-Captura.js
@@ -3,6 +3,14 @@
 const $divsEventos = document.querySelectorAll('.eventos-flujo div');
 console.log($divsEventos)
 function flujoEventos(e){
-    console.log('Hola')
+    console.log(`Hole te saludar ${this.className} desde el ${e.target.className}`)
 }
-$divsEventos.forEach(div => div.addEventListener('click', flujoEventos))
+//En este ejemplo podemos ver el efecto burbuja , al criar los eventos click de las seguintes div, las que estan dentro de un father con evento click, al ser acionadas consecutivamente acionan a los eventos que estan en su contenedor father
+$divsEventos.forEach(div => {
+    // el addEventListener tiene un tercer parametro que especifica el modo del evento, se no le asgnamos valor o le asgnamos "false", coresponde a el modo burbuja como a seguir
+    div.addEventListener('click', flujoEventos,false);
+    // Se le asgnamos "true" so modo passa a ser captura ques funciona de forma contraria del burbuja
+    div.addEventListener('click', flujoEventos,true);
+})
+
+// el modo Captura como su nombre diz captura los eventos de contenedor father al hijo como contracion, contrario de lo burbuja que es en forma de expanción
