b628e9a2de026d9e219e21274d3490382f7aaef5
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-08-29 17:33:40 +0200

    81-Menu-Hamburg terminado

--- 81-Menu-Hamburg.js
+++ 81-Menu-Hamburg.js
@@ -1,10 +1,16 @@
-const $section = document.querySelectorAll('section');
+const $linkSection = document.querySelectorAll('#menu-hamburg button');
 document.addEventListener('click',(e)=>{
     if(e.target.matches('main #button-hamburg')){
      menuHamburg();
     }
     selectEjercicio(e)
 })
+$linkSection.forEach(but=>{
+    but.addEventListener('click',(e)=>{
+       
+    
+    });
+});
 const menuHamburg = function(){
     const menuHamburg = document.getElementById('menu-hamburg')
     console.log(menuHamburg.style.top)
@@ -14,27 +20,48 @@ const menuHamburg = function(){
      menuHamburg.style.top = '';
     };
 };
-
 const selectEjercicio = function(e){
-    let top = 0;
-
-    console.log(e.target)
+    const $sections = document.querySelectorAll('section');
      switch(e.target.id){
         case 'eje1':
-        $section[2]'
+            scroll({
+                top: 0,
+                left: 0,
+                behavior: "smooth",
+              })
+              menuHamburg();
         break;
         case 'eje2':
-      
+            scroll({
+                top: 600,
+                left: 0,
+                behavior: "smooth",
+              })
+              menuHamburg();
         break;
         case 'eje3':
-
+            scroll({
+                top: 1210,
+                left: 0,
+                behavior: "smooth",
+              });
+              menuHamburg();
         break;
         case 'eje4':
-
+            scroll({
+                top: 1817,
+                left: 0,
+                behavior: "smooth",
+              });
+              menuHamburg();
         break;
         case 'eje5':
-
+            scroll({
+                top: 2433,
+                left: 0,
+                behavior: "smooth",
+              });
+              menuHamburg();
         break;
-
-     }
+}
 }
