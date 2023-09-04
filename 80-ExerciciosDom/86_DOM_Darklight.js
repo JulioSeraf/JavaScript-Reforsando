d234e101c73104d68f442c16a25e4bc809dc6bfd
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-09-04 18:31:00 +0200

    89_DOM_API_localStorage

--- 80-ExerciciosDom/86_DOM_Darklight.js
+++ 80-ExerciciosDom/86_DOM_Darklight.js
@@ -1,20 +1,34 @@
 const darkLigth = (btn)=>{
     const btnDarkLight = document.querySelector(btn),
-        d = document;
+        d = document,
+        lS = localStorage;
+        const temaLigth = ()=>{  
+            document.body.classList.remove('bodyDark');
+            btnDarkLight.classList.remove('btnDark');
+            btnDarkLight.querySelector('img').setAttribute('src','img/eclipse.png');  
+        };
+        const temaDark = ()=>{ 
+            document.body.classList.add('bodyDark');
+            btnDarkLight.classList.add('btnDark');
+            btnDarkLight.querySelector('img').setAttribute('src','img/sol.png');
+        };
         d.addEventListener('click',(e)=>{
             if(e.target.matches(btn) || e.target.matches(`${btn} *`)){
-                document.body.classList.toggle('bodyDark');
-                btnDarkLight.classList.toggle('btnDark');
                 if(btnDarkLight.classList.contains('btnDark')){
-                   btnDarkLight.querySelector('img').setAttribute('src','img/eclipse.png');  
+                    lS.setItem('thema','ligth');
+                  temaLigth();
                 }else{
-                    btnDarkLight.querySelector('img').setAttribute('src','img/sol.png');
+                    lS.setItem('thema','dark');
+                   temaDark();
                 }
-                
-                
-            }
-        } )
-
-}
+            };
+        });
+        // localstorage para thema dark/light
+        d.addEventListener('DOMContentLoaded',()=>{
+            if(lS.getItem('thema') === null) lS.setItem('thema','light');
+            if(lS.getItem('thema')==='light') temaDark();
+            if(lS.getItem('thema') === 'dark') temaDark();
+        })
+};
 
 export default darkLigth;
