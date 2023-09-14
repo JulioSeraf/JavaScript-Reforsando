28baece61a4159a17e2128fb4cb1d255de93b3f1
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-09-14 19:25:19 +0200

    105-AJAX-introducción

---
+++ 105-AJAX-introducción.js
@@ -0,0 +1,42 @@
+//AJAX significa Asynchronous JavaScript And XML. En pocas palabras, es el uso del objeto XMLHttpRequest para comunicarse con los servidores.
+
+// XML es praticamente obsoleto
+
+// Puede enviar y recibir información en varios formatos, incluidos JSON, XML, HTML y archivos de texto.
+
+// El atractivo de AJAX es su naturaleza "asíncrona", lo que significa que puede comunicarse con el servidor, intercambiar datos y actualizar la página sin tener que recargar el navegador.
+
+// Hay tres metodos para trabajar con AJAX
+
+// El primero es ActiveXObject 
+   // Ya se encontra obsoleto( era usado para internet explore 8 y inferiores)
+
+//El segundo es XMLHttpRequest 
+    // Es la forme que los navegadores empezaron a usar 
+
+// API de fetch 
+    // Es una forma mas moderna de usar AJAX
+
+
+//Librerias externas de AJAX
+ //jQuerry.ajax()
+ // Axios
+
+//  AJAX no es una tecnología en sí mismo. En realidad, se trata de varias tecnologías independientes que se unen:
+
+// HTML y CSS, para crear una presentación basada en estándares.
+// DOM, para la interacción y manipulación dinámica de la presentación.
+// HTML, XML y JSON, para el intercambio y la manipulación de información.
+// XMLHttpRequest o Fetch, para el intercambio asíncrono de información.
+// JavaScript, para unir todas las demás tecnologías.
+
+// Es importante también considerar los Códigos de estado de respuesta HTTP y los estados de la petición AJAX:
+
+// Estado	                  Valor
+// READY_STATE_UNINITIALIZED	0  => estado de no inicializacion 
+// READY_STATE_LOADING	        1  => estado de cargando
+// READY_STATE_LOADED	        2  => estado de cargado 
+// READY_STATE_INTERACTIVE	    3  => estado enteractivo
+// READY_STATE_COMPLETE	        4  => terminado el processo de petticion
+
+
