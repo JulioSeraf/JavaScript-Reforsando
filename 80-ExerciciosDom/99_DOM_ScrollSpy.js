04972feac5db1390d28abf0367273aa88aa2104f
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-09-14 16:48:37 +0200

    99_DOM_ScrollSpy error corrigido

--- 80-ExerciciosDom/99_DOM_ScrollSpy.js
+++ 80-ExerciciosDom/99_DOM_ScrollSpy.js
@@ -3,15 +3,10 @@ export default function scrollSpy(){
     d.addEventListener('DOMContentLoaded',()=>{
         const viewSections = d.querySelectorAll('main section'),
             menuHamburg = d.querySelectorAll('#menu-hamburg a');
-            console.log(menuHamburg)
         const obeservar = new IntersectionObserver((entries)=>{
             entries.forEach((entry) =>{
                 if(entry.isIntersecting){
-                    // viewSections.forEach((el, index) => )
-                    // menuHamburg.querySelector('#'+ entry.target.id).classList.add('painelSpy');
-                }else{
-                    viewSections.forEach((el, index) => menuHamburg[index].classList.remove('painelSpy'))
-                    // menuHamburg.querySelector('#'+ entry.target.id).classList.remove('painelSpy');
+                    menuHamburg.forEach(el => (entry.target.id === el.dataset.sec) ? el.classList.add('painelSpy'):el.classList.remove('painelSpy')) 
                 };
             });
             
