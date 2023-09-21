e6cf8256a76e1517ba49f3f841a4a4eae9a769bd
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-09-21 07:21:58 +0200

    Criados estrutura html para exercicios de crud

---
+++ 113_Consumos_de_datos_con_clietesRest.js
@@ -0,0 +1,4 @@
+//obtenemos nuestro datos via GET
+//hacemos un isert en los datos obtenidos con POST
+//Podemos hacer un update en los datos con PUT
+//E podemos elimiar elemetos de los datos con el DELETE
