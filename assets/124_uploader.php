5322891a3cc3908151f937c1984abb2b809e0b8c
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-10-02 19:16:52 +0200

    125_update-DragAndDrop

--- assets/124_uploader.php
+++ assets/124_uploader.php
@@ -1,31 +1,30 @@
 <?php
-echo "HOLA BRO";
-var_dump($_FILES);
- // el metodo isset() en PHP valida se una variabel existe
-// if(isset($_FILES["file"])){
-//     // criamos un variabel en PHP usando el ($), no usamos const o let
-//     $name = $_FILES["file"]["name"];
-//     $file = $_FILES["file"]["tmp_name"];
-//     $erro = $_FILES["file"]["error"];
-//     $destination = "../assets/files/$name";
+// echo "HOLA BRO";
+// var_dump($_FILES);
+//  el metodo isset() en PHP valida se una variabel existe
+if(isset($_FILES["file"])){
+    // criamos un variabel en PHP usando el ($), no usamos const o let
+    $name = $_FILES["file"]["name"];
+    $file = $_FILES["file"]["tmp_name"];
+    $erro = $_FILES["file"]["error"];
+    $destination = "../assets/files/$name";
 
-//     $uploand = move_uploaded_file($file,$destination);
-//    if($uploand){
-//     $res = array(
-//         "err" => false,
-//         "status"=> http_response_code(200),
-//         "statusText" => "Archivo $name subido con éxito",
-//         "files" => $_FILES["file"]
-//     );
-//    }else{
-//     $res = array(
-//         "err" => true,
-//         "status"=> http_response_code(400),
-//         "statusText" => "Error al subir el archivo $name",
-//         "files" => $_FILES["file"]
-//     );
-//    };
+    $uploand = move_uploaded_file($file,$destination);
+   if($uploand){
+    $res = array(
+        "err" => false,
+        "status"=> http_response_code(200),
+        "statusText" => "Archivo $name subido con éxito",
+        "files" => $_FILES["file"]
+    );
+   }else{
+    $res = array(
+        "err" => true,
+        "status"=> http_response_code(400),
+        "statusText" => "Error al subir el archivo $name",
+        "files" => $_FILES["file"]
+    );
+   };
+   echo json_encode($res);
+};
 
-//    echo json_encode($res)
-
-}
