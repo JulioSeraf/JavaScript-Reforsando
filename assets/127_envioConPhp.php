c39fcdc25f5e497fff5b9279ead0a931b03655cf
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-10-04 17:31:50 +0200

    127_AJAX-PHP-EnvíoFormularioconFetch
    terminado

--- assets/127_envioConPhp.php
+++ assets/127_envioConPhp.php
@@ -2,6 +2,7 @@
 //Executamos el codigo solo cuando la variable de tipo Post este definida, isset() es un metodo de php para detectar si la variabel existe
 
 if(isset($_POST)){
+    error_reporting(0); // el error reporting funciona para manipular los error, se les asignamos [0] nos nos reportara error en la parte de font
     // guardamos todas la variables que viene en nustro form(name="") en variables de php() lambrando que para criar variables en php usamos $ al principio
     $name = $_POST["name"];
     $email = $_POST["email"];
@@ -27,7 +28,7 @@ if(isset($_POST)){
 
     if($send_mail){
         $res =[
-            "err"= false,
+            "err"=> false,
             "message"=> "Tus datos han sidos enviados"
         ];
     }else{
@@ -36,7 +37,14 @@ if(isset($_POST)){
             "message" => "Error al enviar tus datos, intenta nuevamente"
         ];
     };
+    // header('Access-Control-Allow-Origin:*'); // el (*) indica que podemos recibir peticiones de caulquer lado
+
+    // header('Access-Control-Allow-Origin:http://Juliogithub.com'); // al passar un dominio unico, solo se podera reciber peticiones de tal dominio
+
     header("Content-type:application/json");
-    echo json-encode($res);
+    echo json_encode($res);
     exit;
 };
+
+// CORS = INTERCAMBIO DE ORIGENS DE RECURSOS CRUZADOS:
+// EL coRS es un politica de normas que dice que "todas as peticiones ajax en teoria tenia que estar en el mismo servidor", para poder activar el mode cors, lo devemos ativar en el front, en la option de envio de ajax, y en el back, por via de las header
