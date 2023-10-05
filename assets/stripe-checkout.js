514ddb5d92ceed3b9fab271c4c910fb74d98343c
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-10-05 18:07:29 +0200

    Update stripe-checkout.js

--- assets/stripe-checkout.js
+++ assets/stripe-checkout.js
@@ -1,33 +1,18 @@
 const d = document,
     $shake = d.getElementById('shake'),
     $template = d.getElementById('milk-template').contain,
-    $fragmento = d.createDocumentFragment();
-    const clone =
+    $fragmento = d.createDocumentFragment(),
+    fetchOptions = {
+        headers:{//usamos la option header con la propriesas authorization: bearer (mais la llave secreta de stripe) para acesseder a los productos, sin essa autorizacion nos lanza un error 401(no autorizado)
+            authorization:"Bearer (llave secreta aqui)"
+        }
+    };
+    let prices,
+        productos;
+    // Para hacer varias peticiones de un sola vez usamos el metodo all() del Objeto Promise, 
+    Promise.all(
+        fetch("https://api.stripe.com/v1/products",fetchOptions),
+        fetch("https://api.stripe.com/v1/prices",fetchOptions))
+        .then(res => Promise.all(response.map(res => res.json())))
+        .then(json => console.log(json))
                                                   //Peticon de Productos 
-fetch("https://api.stripe.com/v1/products",({
-    headers:{//usamos la option header con la propriesas authorization: bearer (mais la llave secreta de stripe) para acesseder a los productos, sin essa autorizacion nos lanza un error 401(no autorizado)
-        authorization:"Bearer (Llave secreta aqui)"
-    }
-})).then(res => res.ok?res.json():Promise.reject(res))
-.then(json =>{
-    json.data.forEach(producto => {
-        // console.log(producto.images[0])
-        $shake.querySelector('img').src = producto.images[0],
-        $shake.querySelector('figcaption').textContent = producto.name,
-        $template.appendChild($shake);
-        let $clone = d.importNode($template,true);
-        $fragmento.appendChild($clone);
-    });
-    d.getElementById("milkshake").appendChild($fragmento)
-})
-.catch(err => {
-    let errMansage = err.statusText || "Detectado"
-
-})
-
-                                                //Peticion de precios 
-fetch("https://api.stripe.com/v1/prices",({
-    headers:{//usamos la option header con la propriesas authorization: bearer (mais la llave secreta de stripe) para acesseder a los productos, sin essa autorizacion nos lanza un error 401(no autorizado)
-        authorization:"Bearer (Llave secreta aqui)"
-    }
-})).then(res => res.ok?res.json():Promise.reject(res)).then(res => console.log(res))
